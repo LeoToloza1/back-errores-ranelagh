667a097b450d7e9eb2b6ad1f4b060f2c5b7c96d9
Leotoloza1 <leotoloza6@gmail.com>
2026-02-03 13:36:24 -0300

    fix: errores no mandaba peticion

--- src/controller/ErroresController.ts
+++ src/controller/ErroresController.ts
@@ -1,15 +1,37 @@
+import { Request, Response } from "express";
 import { ErroresService } from "../services/ErroresService.js";
 import { IError } from "../interfaces/IError.js";
+import { Notificacion } from "../interfaces/Notificacion.js";
 
 export class ErroresController {
     constructor(private service: ErroresService) { }
 
-    async registrarError(error: IError) {
+    registrarError = async (req: Request, res: Response): Promise<void> => {
         try {
-            const errorGuardado = await this.service.registrarError(error);
-        } catch (error) {
-            console.error("Error al registrar error:", error);
-            throw new Error("no hay personal cargado o hubo un error");
+            const datosError: IError = req.body;
+            await this.service.registrarError(datosError);
+
+            const respuestaExito: Notificacion = {
+                tipo: "success",
+                titulo: "Operación Exitosa",
+                posicion: "top-right",
+                autoClose: "3000",
+                mensaje: "El error ha sido registrado y notificado correctamente."
+            };
+
+            res.status(201).json(respuestaExito);
+
+        } catch (error: any) {
+            console.error("Controller Error:", error.message);
+            const respuestaError: Notificacion = {
+                tipo: "error",
+                titulo: "Error de Registro",
+                posicion: "top-right",
+                autoClose: "5000",
+                mensaje: error.message || "No se pudo procesar la solicitud."
+            };
+
+            res.status(400).json(respuestaError);
         }
     }
 }
